1bb9f0988834f604686434769db6bd3551fbdf0a
KadamTeju <tejushinde8897@gmail.com>
2023-10-02 20:35:18 +0530

    UC2 Complete.

--- UserRegistrationDay23/src/main/java/welcometouser/FirstNameValidation.java
+++ UserRegistrationDay23/src/main/java/welcometouser/FirstNameValidation.java
@@ -3,23 +3,24 @@ package welcometouser;
 import welcometouser.NameValidator;
 public class FirstNameValidation {
     public static void main(String[] args) {
-        // Lambda expression for validating the first name
-        NameValidator firstNameValidator = (name) -> {
-            return name.matches("[A-Z][a-zA-Z]{2,}");
-        };
+            // Lambda expression for validating the first name
+            NameValidator firstNameValidator = (name) -> {
+                // Check if the name starts with a capital letter and has a minimum length of 3 characters
+                return name.matches("[A-Z][a-zA-Z]{2,}");
+            };
 
-        // Get user input for the first name
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Enter your first name: ");
-        String firstName = scanner.nextLine();
+            // Get user input for the first name
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            System.out.print("Enter your first name: ");
+            String firstName = scanner.nextLine();
 
-        // Validate the first name using the lambda expression
-        if (firstNameValidator.validate(firstName)) {
-            System.out.println("Valid first name.");
-        } else {
-            System.out.println("Invalid first name. First name should start with a capital letter and have a minimum of 3 characters.");
+            // Validate the first name using the lambda expression
+            if (firstNameValidator.validate(firstName)) {
+                System.out.println("Valid first name.");
+            } else {
+                System.out.println("Invalid first name. First name should start with a capital letter and have a minimum of 3 characters.");
+            }
+
+            scanner.close();
         }
-
-        scanner.close();
-    }
 }
